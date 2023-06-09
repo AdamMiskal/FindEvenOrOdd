a900ecad16f980ab71a3e47e510a1f395e310b72
Adam Miskal <adam.nevol@gmail.com>
2023-06-10 01:16:51 +0300

    Add project files.

---
+++ FindEvenOrOdd/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindEvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            bool isValidInput = false;
+
+            
+
+
+            do
+            {
+                Console.Write("Please enter a number: ");
+
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            } while (!isValidInput);
+
+
+            if (number %2==0)
+            {
+                Console.WriteLine($"the {number}  is an even number");
+            }
+            else
+            {
+                Console.WriteLine($"the {number}  is an odd number");
+            }
+        }
+    }
+}
+
